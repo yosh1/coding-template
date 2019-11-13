75593454f5844a89c8cc2529687495bb0779973b
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-11-13 12:08:57 +0900

    🤩(env): gulp -> webpack

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const AutoPrefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = [{
+    entry: {
+        app: [
+            './src/js/app.js',
+            './src/sass/style.scss',
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js',
+        publicPath: '/',
+    },
+    module: {
+        rules: [{
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                }, {
+                    loader: 'css-loader',
+                }, {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: [
+                            AutoPrefixer({
+                                browsers: ['last 2 versions', 'Android >= 4'],
+                            }, ),
+                        ],
+                    },
+                }, {
+                    loader: 'sass-loader',
+                }],
+            },
+            {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                use: [{
+                    loader: 'url-loader?limit=100000&name=img/[name].[ext]',
+                }, ],
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCSSAssetsPlugin(),
+            new UglifyJsPlugin(),
+        ],
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: path.resolve(__dirname, 'src/img/'),
+            to: path.resolve(__dirname, 'dist/img/'),
+        }, ]),
+        new MiniCssExtractPlugin({
+            filename: 'css/style.css',
+        }),
+        new ImageminPlugin({
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            pngquant: {
+                quality: '95-100',
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html')
+        })
+    ],
+    devServer: {
+        contentBase: './dist',
+        watchContentBase: true,
+        port: 3000,
+        open: true,
+    },
+}, ];
