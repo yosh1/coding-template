eab72832252ff867c8e776705fea659766236074
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-11-21 16:39:37 +0900

    ✨(env): gulp and babel

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,67 +1,38 @@
+'use strict';
+
 import gulp from 'gulp';
-import browserify from 'browserify';
-import babelify from 'babelify';
-import watchify from 'watchify';
-import buffer from 'vinyl-buffer';
-import source from 'vinyl-source-stream';
+import sass from 'gulp-sass';
+import babel from 'gulp-babel';
 import browserSync from 'browser-sync';
-import gulpLoadPlugins from 'gulp-load-plugins';
-const $ = gulpLoadPlugins();
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
 
-function bundle(watching = false) {
-  // `src/scripts/main.js` を `assets/scripts.js` にビルド
-  const b = browserify({
-    entries: ['src/scripts/main.js'],
-    transform: ['babelify'],
-    debug: true,
-    plugin: (watching) ? [watchify] : null
-  })
-  .on('update', () => {
-    bundler();
-    console.log('scripts rebuild');
-  });
-
-  function bundler() {
-    return b.bundle()
-      .on('error', (err) => {
-        console.log(err.message);
-      })
-      .pipe(source('scripts.js'))
-      .pipe(buffer())
-      .pipe($.sourcemaps.init({loadMaps: true}))
-      .pipe($.uglify())
-      .pipe($.sourcemaps.write('./'))
-      .pipe(gulp.dest('assets/'));
-  }
-
-  return bundler();
-}
-
-gulp.task('scripts', () => {
-  bundle();
+gulp.task('babel', function () {
+  gulp.src('src/js/*.js')
+    .pipe(babel({
+      presets: ["@babel/preset-env"]
+    }))
+    .pipe(uglify())
+      .pipe(rename({
+        extname: '.min.js'
+      }))
+    .pipe(gulp.dest('assets/js/'));
 });
 
-gulp.task('styles', () => {
-  // `src/styles/**.scss` を `assets/` にビルド
-  gulp.src(`src/styles/**/*.scss`)
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
-    .pipe($.autoprefixer())
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('assets'))
+
+gulp.task('scss', () => {
+      gulp.src("src/scss/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("assets/css"));
 });
 
-gulp.task('build', ['scripts', 'styles']);
+gulp.task('build', gulp.parallel('babel', 'scss'));
 
 gulp.task('watch', () => {
-  bundle(true);
-  gulp.watch('src/styles/**/*.scss', ['styles']);
+  gulp.watch('src/**/*', gulp.parallel('babel', 'scss'));
 });
 
-gulp.task('serve', ['watch'], () => {
+gulp.task('serve', gulp.task('watch'), () => {
   browserSync({
     notify: false,
     port: 9000,
@@ -73,10 +44,9 @@ gulp.task('serve', ['watch'], () => {
 
   gulp.watch([
     '**/*.html',
-    '**/*.php',
     'assets/**/*.css',
     'assets/**/*.js'
   ]).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['build', 'serve']);
+gulp.task('default', gulp.parallel('build', 'serve'));
--- src/scripts/sample.js
+++
@@ -1,4 +0,0 @@
-export default () => {
-  const name = 'ES2016';
-  console.log(`hello ${name}!!!`);
-};
